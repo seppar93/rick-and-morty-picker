4399f7de64bff62b055b95e2e96f656f8a79f392
Sepehr Parirokh <sepehrparirokh@gmail.com>
2022-03-16 19:04:50 -0400

    fetch data

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Store, StoreProvider } from './Store';
@@ -7,13 +7,34 @@ import { Store, StoreProvider } from './Store';
 
 
 function App() {
-  const store = useContext(Store)
+  const {state,dispatch} = useContext(Store)
   console.log(store);
+  
+  const fetchDataAction = async () => {
+    const url = 'https://api.tvmaze.com/singlesearch/shows?q=rick-&-morty&embed=episodes'
+    const response = await fetch(url)
+    const data = await response.json()
+    return dispatch({
+      type: "FETCH_DATA",
+      payload: data._embedded.episodes
+    })
+  }
+
+  useEffect(() => {
+    fetchDataAction()
+
+  }, [])
+  
   
   return (
     <div className="App">
       <h1>Rick and Morty!!</h1>
       <p>Pick your Favorite episode!</p>
+      <section>
+        {state.episodes.map((episode) => {
+          <section key={episode.id}></section>
+        })}
+      </section>
     </div>
   );
 }
--- src/Store.tsx
+++ src/Store.tsx
@@ -1,15 +1,41 @@
-import  { createContext } from 'react'
+import  { createContext, useReducer } from 'react'
 
-
-export const Store = createContext();
-const initialState = {
-
+interface IAction {
+    type:string,
+    payload: any
 }
 
-function reducer() {
+interface IState {
+    episodes: [],
+    favorites: []
+}
+
+const initialState:IState = {
+    // initial state is a the generic passed to createContext
+    episodes: [],
+    favorites: []
+}
+
+export const Store = createContext<IState>(initialState);
+
+
+
+function reducer(state: IState,action:IAction) {
+    switch(action.type){
+        case 'FETCH_DATA': 
+            return {
+                ...state,
+                episodes: action.payload
+            }
+        default:
+            return state;
+    }
     // pass
 }
 
 export function StoreProvider(props): JSX.Element{
-    return <Store.Provider value='test'> {props.children} </Store.Provider>
+    
+    const [state, dispatch] = useReducer(reducer, initialState)
+
+    return <Store.Provider value={{state, dispatch}}> {props.children} </Store.Provider>
 }
