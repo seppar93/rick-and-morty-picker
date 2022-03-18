ea250a913976feb29a26a04b4d7e99e14a7a4ca7
Sepehr Parirokh <sepehrparirokh@gmail.com>
2022-03-18 12:39:44 -0400

    update action type with enum

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,73 @@
-import React, { useContext, useEffect } from 'react';
-import logo from './logo.svg';
+import React, { useCallback, useContext, useEffect } from 'react';
 import './App.css';
-import { Store, StoreProvider } from './Store';
+import { Actions, IEpisode, Store} from './Store';
 
-interface IEpisode {
-  id:number
-  image:string
-  number:number
-  season: number
-  url:string
-}
+
 
 function App() {
   const { state, dispatch } = useContext(Store);
 
-  const fetchDataAction = async () => {
+  const fetchDataAction = useCallback( async () => {
     const url =
-      'https://api.tvmaze.com/singlesearch/shows?q=morty&embed=episodes';
-    const response = await fetch(url);
-    const data = await response.json();
-    console.log("DATA",data );
-    
-    return dispatch({
-      type: 'FETCH_DATA',
-      payload: data._embedded.episodes,
-    });
-  };
+    'https://api.tvmaze.com/singlesearch/shows?q=morty&embed=episodes';
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log('DATA', data);
+
+  return dispatch({
+    type: Actions.FETCH_DATA,
+    payload: data._embedded.episodes,
+  });
+  },
+
+    [dispatch],
+  )
+
+
+  // const fetchDataAction = async () => {
+    // const url =
+    //   'https://api.tvmaze.com/singlesearch/shows?q=morty&embed=episodes';
+    // const response = await fetch(url);
+    // const data = await response.json();
+    // console.log('DATA', data);
+
+    // return dispatch({
+    //   type: Actions.FETCH_DATA,
+    //   payload: data._embedded.episodes,
+    // });
+  // };
 
   useEffect(() => {
     const checkSession = sessionStorage.getItem('episodes');
-    // if (checkSession) {
-    //   return dispatch({
-    //     type: 'SET_DATA',
-    //     payload: JSON.parse(checkSession),
-    //   });
-    // }
+    if (checkSession) {
+      return dispatch({
+        type: 'SET_DATA',
+        payload: JSON.parse(checkSession),
+      });
+    }
 
     fetchDataAction();
-  }, []);
+  }, [dispatch, fetchDataAction]);
 
   // console.log(state);
-  
+
   return (
     <div className='App'>
-      <h1>Rick and Morty!!</h1>
-      <p>Pick your Favorite episode!</p>
-      <section>
-        {state.episodes.map((episode) => {
+      <header className='header'>
+        <h1>Rick and Morty!!</h1>
+        <p>Pick your Favorite episode!</p>
+      </header>
+      <section className='episode-layout'>
+        {state.episodes.map((episode:IEpisode) => {
           return (
-
-            <section key={episode.id}>
+            <section className='episode-box' key={episode.id}>
               <img src={episode.image.medium} alt={episode.name} />
               <div>{episode.name}</div>
               <section>
                 Season:{episode.season} Number: {episode.number}
               </section>
-              </section>
-          )
+            </section>
+          );
         })}
       </section>
     </div>
--- src/Store.tsx
+++ src/Store.tsx
@@ -1,41 +1,66 @@
-import  { createContext, useReducer } from 'react'
+import { createContext, useReducer } from 'react';
 
-interface IAction {
-    type:string,
-    payload: any
+export interface IEpisode {
+  id: number;
+  image: {
+    medium: string;
+  };
+  number: number;
+  season: number;
+  url: string;
+  name: string;
 }
+
+export enum Actions {
+  FETCH_DATA,
+  SET_DATA,
+  SET_FAV_DATA,
+}
+
+type ActionType =
+  | { type: Actions.FETCH_DATA; payload: [] }
+  | { type: Actions.SET_DATA; payload: IEpisode }
+  | { type: Actions.SET_FAV_DATA; payload: IEpisode };
 
 interface IState {
-    episodes: [],
-    favorites: []
+  episodes: [];
+  favorites: IEpisode[];
 }
 
-const initialState:IState = {
-    // initial state is a the generic passed to createContext
-    episodes: [],
-    favorites: []
+const initialState: IState = {
+  // initial state is a the generic passed to createContext
+  episodes: [],
+  favorites: [],
+};
+
+export const Store = createContext<IState>(initialState);
+
+function reducer(state: IState, action: ActionType):IState {
+  switch (action.type) {
+    case Actions.FETCH_DATA:
+      return {
+        ...state,
+        episodes: action.payload,
+      };
+
+    case Actions.SET_FAV_DATA:
+      return {
+        ...state,
+        favorites: [...state.favorites, action.payload],
+      };
+
+    default:
+      return state;
+  }
 }
 
-export const Store = createContext<IState | any>(initialState);
+export function StoreProvider(props): JSX.Element {
+  const [state, dispatch] = useReducer(reducer, initialState);
 
-
-
-function reducer(state: IState,action:IAction) {
-    switch(action.type){
-        case 'FETCH_DATA': 
-            return {
-                ...state,
-                episodes: action.payload
-            }
-        default:
-            return state;
-    }
-    // pass
-}
-
-export function StoreProvider(props:any): JSX.Element{
-    
-    const [state, dispatch] = useReducer(reducer, initialState)
-
-    return <Store.Provider value={{state, dispatch}}> {props.children} </Store.Provider>
+  return (
+    <Store.Provider value={{ state, dispatch }}>
+      {' '}
+      {props.children}{' '}
+    </Store.Provider>
+  );
 }
